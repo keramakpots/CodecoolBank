88860e43e6c9b5d4e2f1667ac1b79b7653346ade
Marek Stopka <keramakpots@gmail.com>
2017-06-06 14:48:19 +0200

    Create account interface

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -2,4 +2,10 @@ package model;
 
 public interface Account {
 
+    public void deposit(double amount);
+
+    public void withdraw(double amount);
+
+    public double getBalance(); public int getAccountId();
+
 }
