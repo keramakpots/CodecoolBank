0e8af22fd3e65d572b59f3cdec5fa93276e99b54
Marek Stopka <keramakpots@gmail.com>
2017-06-06 14:54:18 +0200

    Add interface method implementation

--- src/main/java/model/AbstractAccount.java
+++ src/main/java/model/AbstractAccount.java
@@ -1,5 +1,19 @@
 package model;
 
 public abstract class AbstractAccount implements Account {
+    public void deposit(double amount) {
 
+    }
+
+    public void withdraw(double amount) {
+
+    }
+
+    public double getBalance() {
+        return 0;
+    }
+
+    public int getAccountId() {
+        return 0;
+    }
     }
