605b715f27c2d9f9f0a726f533ce56f5b73c70c6
Marek Stopka <keramakpots@gmail.com>
2017-06-07 10:14:23 +0200

    Add test for checking AccountDaoImpl.getAll() method

--- src/test/java/model/SavingAccountTest.java
+++ src/test/java/model/SavingAccountTest.java
@@ -2,12 +2,29 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import DAO.AccountDaoImpl;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 
 class SavingAccountTest {
+    private String pathToDB = "jdbc:sqlite:src/main/resources/Bank";
+    private Connection connection;
     @Test
     void isSavingAccountInheritFromAbstractAccount() {
         assertTrue(SavingAccount.class.getSuperclass().equals(AbstractAccount.class));
     }
+
+    @Test
+    void isGetAllTakesListOfSavingAccounts() throws SQLException {
+        connection = DriverManager.getConnection(pathToDB);
+        List<SavingAccount> account = new ArrayList<SavingAccount>();
+        assertTrue(account.getClass().equals(new AccountDaoImpl(connection).getAll().getClass()));
+    }
+
+
 }
