dfd5de1a881e5197044fab5415f448378e45e701
oskardz <oskardz1991@gmail.com>
2017-06-07 14:32:48 +0200

    Add exception to throw when consumer is already disactivated

---
+++ src/main/java/exceptions/AlreadyDisactivatedException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+/**
+ * Created by oskar on 07.06.17.
+ */
+public class AlreadyDisactivatedException extends RuntimeException {
+    public AlreadyDisactivatedException() {
+    }
+}
