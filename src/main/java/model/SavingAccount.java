f997d1b04d63fce8c1c76ddedb3651b46bd55a43
Marek Stopka <keramakpots@gmail.com>
2017-06-07 10:14:59 +0200

    Add fields and contructor

--- src/main/java/model/SavingAccount.java
+++ src/main/java/model/SavingAccount.java
@@ -1,6 +1,33 @@
 package model;
 
 
-public class SavingAccount extends AbstractAccount {
+import java.math.BigInteger;
+import java.sql.Date;
 
+public class SavingAccount extends AbstractAccount {
+    private Integer accountID;
+    private Integer customerID;
+    private String number;
+    private Integer accountTypeID;
+    private Integer accountStatusID;
+    private Date openDate;
+    private java.math.BigInteger balance;
+    private BigInteger debitLine;
+    private Integer interest;
+
+    public SavingAccount(Integer accountID, Integer customerID, String number,
+        Integer accountTypeID,
+        Integer accountStatusID, Date openDate, BigInteger balance, BigInteger debitLine,
+        Integer interest) {
+        super();
+        this.accountID = accountID;
+        this.customerID = customerID;
+        this.number = number;
+        this.accountTypeID = accountTypeID;
+        this.accountStatusID = accountStatusID;
+        this.openDate = openDate;
+        this.balance = balance;
+        this.debitLine = debitLine;
+        this.interest = interest;
+    }
 }
