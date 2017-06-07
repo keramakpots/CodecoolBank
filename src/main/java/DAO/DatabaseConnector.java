8d5e81b2a94c3e7fde8ec8d12f99dc5186ffd6ca
Marek Stopka <keramakpots@gmail.com>
2017-06-07 10:22:14 +0200

    Add databaseConnector

---
+++ src/main/java/DAO/DatabaseConnector.java
@@ -0,0 +1,32 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnector {
+
+    public Connection connect(String path) {
+        Connection connection = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(path);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        return connection;
+    }
+
+    public static void disconnect(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("Database closing failed");
+            }
+        }
+    }
+
+}
