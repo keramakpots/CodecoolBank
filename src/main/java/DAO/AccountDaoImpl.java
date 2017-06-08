e53c4592d7dc258cbd2aa4bd035f08ab0f9b5e73
oskardz <oskardz1991@gmail.com>
2017-06-08 14:32:49 +0200

    Add AccoutController

--- src/main/java/DAO/AccountDaoImpl.java
+++ src/main/java/DAO/AccountDaoImpl.java
@@ -1,5 +1,10 @@
 package DAO;
 
+import model.Account;
+import model.AccountStatus;
+import model.AccountType;
+import model.Customer;
+
 import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.Date;
@@ -7,10 +12,6 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import model.Account;
-import model.AccountStatus;
-import model.AccountType;
-import model.Customer;
 
 public class AccountDaoImpl implements AccountDao {
 
@@ -130,34 +131,5 @@ public class AccountDaoImpl implements AccountDao {
             System.err.println(e.getClass().getName() + ":AccountDaoImpl " + e.getMessage());
             e.printStackTrace();
         }
-    }
-
-    public Account findByNumber(String number) {
-        Statement stmt;
-        Account account = null;
-        try {
-            stmt = connection.createStatement();
-            ResultSet rs = stmt
-                .executeQuery("SELECT * FROM Accounts WHERE Number = '" + number + "';");
-            Integer accountID = rs.getInt("AccountID");
-            Integer customerID = rs.getInt("CustomerID");
-            Customer customer = customerDao.find(customerID);
-            Integer accountTypeID = rs.getInt("AccountTypeID");
-            AccountType accountType = accountTypeDao.find(accountTypeID);
-            Integer accountStatusID = rs.getInt("AccountStatusID");
-            AccountStatus accountStatus = accountStatusDao.find(accountStatusID);
-            Date openDate = Date.valueOf(rs.getString("OpenDate"));
-            BigInteger balance = BigInteger.valueOf(rs.getInt("Balance"));
-            BigInteger debitLine = BigInteger.valueOf(rs.getInt("DebitLine"));
-            Integer interest = rs.getInt("Interest");
-            account = new Account(accountID, customer, number, accountType, accountStatus, openDate,
-                balance, debitLine, interest);
-            rs.close();
-            stmt.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ":AccountDaoImpl " + e.getMessage());
-            e.printStackTrace();
-        }
-        return account;
     }
 }
